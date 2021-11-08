129622e672860c466ee652b90000c0558798281f
Bernardo Raposo <braposo@singlestore.com>
2021-11-08 16:27:32 +0000

    Adds code for koala clubhouse

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Magic Eden Wolves of SOL Street Monitor
+# Koala Clubhouse Monitor
 
-Monitors Wolves of SOL Street marketplace and updates data every 30min.
+Monitors Koala Clubhouse marketplace and updates data every 30min.
 
-Browse data at https://flatgithub.com/braposo/magiceden-wolves-monitor
+Browse data at https://flatgithub.com/braposo/koala-monitor
+
+Check `koala-top-picks` for the best picks under 0.3, 0.6, 1.5 and 4 SOL!
--- postprocess.ts
+++ postprocess.ts
@@ -2,16 +2,6 @@
 // Has helper functions for manipulating csv, txt, json, excel, zip, and image files
 import { readJSON, writeCSV } from "https://deno.land/x/flat@0.0.13/mod.ts";
 
-type Item = {
-  mintAddress: string;
-  price: number;
-  title: string;
-};
-
-type RawData = {
-  results: Array<Item>;
-};
-
 type ParsedData = {
   id: number;
   price: number;
@@ -19,28 +9,64 @@ type ParsedData = {
   url: string;
 };
 
+type Item = {
+  mintAddress: string;
+  price: number;
+  title: string;
+};
+
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0];
-const data: RawData = await readJSON(filename);
-const moonrank: Record<string, string> = await readJSON("wolves-rank.json");
+const data: { results: Array<Item> } = await readJSON(filename);
+const moonrank: Record<string, string> = await readJSON("koala-rank.json");
 
-const enhancedData: Array<ParsedData> = data.results
-  .map((item) => {
-    const [_, id] = item.title.split("#");
-    const url = `https://magiceden.io/item-details/${item.mintAddress}`;
+const enhancedData: Array<ParsedData> = data.results.map((item) => {
+  const [_, id] = item.title.split("#");
+  const url = `https://magiceden.io/item-details/${item.mintAddress}`;
 
-    return {
-      id: parseInt(id),
-      price: item.price,
-      rank: moonrank[id],
-      url,
-    };
-  })
-  .filter(Boolean)
-  .sort((a, b) => a.id - b.id);
+  return {
+    id: parseInt(id),
+    price: item.price,
+    rank: moonrank[id],
+    url,
+  };
+});
 
 console.log("Initial items:", data.results.length);
 console.log("Processed items:", enhancedData.length);
 
-await writeCSV("wolves-data-processed.csv", enhancedData);
+await writeCSV("koala-data-processed.csv", enhancedData);
 console.log("Wrote data");
+
+const sortedData = enhancedData.sort((a, b) => {
+  return parseInt(a.rank) - parseInt(b.rank);
+});
+
+const buckets = sortedData.reduce<Array<Array<ParsedData>>>(
+  (data, item) => {
+    let bucket: number | undefined = undefined;
+    if (item.price <= 0.3) {
+      bucket = 0;
+    } else if (item.price <= 0.6) {
+      bucket = 1;
+    } else if (item.price <= 1.5) {
+      bucket = 2;
+    } else if (item.price <= 4) {
+      bucket = 3;
+    }
+
+    if (bucket !== undefined) {
+      data[bucket].push(item);
+    }
+
+    return data;
+  },
+  [[], [], [], []]
+);
+
+const topPicks = buckets.reduce((picks, bucket) => {
+  const bucketSelection = bucket.slice(0, 3);
+  return [...picks, ...bucketSelection];
+}, []);
+
+await writeCSV("koala-top-picks.csv", topPicks);
