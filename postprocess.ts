e055d7c64d740fbe3100ec5e33a6cc51d86a67dd
Bernardo Raposo <braposo@singlestore.com>
2021-11-08 17:54:28 +0000

    Updates buckets for top picks

--- postprocess.ts
+++ postprocess.ts
@@ -49,11 +49,11 @@ const sortedData = enhancedData.sort((a, b) => {
 const buckets = sortedData.reduce<Array<Array<ParsedData>>>(
   (data, item) => {
     let bucket: number | undefined = undefined;
-    if (item.price <= 0.3) {
+    if (item.price <= 0.6) {
       bucket = 0;
-    } else if (item.price <= 0.6) {
+    } else if (item.price <= 1) {
       bucket = 1;
-    } else if (item.price <= 1.5) {
+    } else if (item.price <= 2) {
       bucket = 2;
     } else if (item.price <= 4) {
       bucket = 3;
